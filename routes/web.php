ec76996cdf1fbd8c2d68b16a9250e3c646908bd7
khamis9 <khamishussein2003@gmail.com>
2025-12-07 18:41:06 +0200

    Remove root package.json to fix Railway build detection

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,15 @@ Route::get('/{any}', function () {
     $indexPath = public_path('index.html');
     
     if (!file_exists($indexPath)) {
-        abort(500, 'Frontend not built. index.html not found at: ' . $indexPath);
+        $publicContents = scandir(public_path());
+        $debugInfo = [
+            'expected_path' => $indexPath,
+            'public_path' => public_path(),
+            'public_contents' => $publicContents,
+            'file_exists' => file_exists($indexPath),
+            'is_readable' => is_readable(public_path()),
+        ];
+        abort(500, 'Frontend not built. Debug info: ' . json_encode($debugInfo));
     }
     
     return response()->file($indexPath);
