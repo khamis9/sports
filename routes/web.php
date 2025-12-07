f69255ca79012b8c6ec8d7d241093b7cb74412a6
khamis9 <khamishussein2003@gmail.com>
2025-12-07 18:31:34 +0200

    Fix React build deployment - improved build process and error handling

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 // Serve React app for all routes (SPA)
 Route::get('/{any}', function () {
-    return file_get_contents(public_path('index.html'));
+    $indexPath = public_path('index.html');
+    
+    if (!file_exists($indexPath)) {
+        abort(500, 'Frontend not built. index.html not found at: ' . $indexPath);
+    }
+    
+    return response()->file($indexPath);
 })->where('any', '.*');
